53dd1d93acd1211e63297ff94937d1af927daeb6
Hongchan Choi <hongchan@chromium.org>
2015-04-17 16:00:14 -0700

    Update 2015-04-17T23:00:11.783Z

--- js/MiniMap.js
+++ js/MiniMap.js
@@ -10,7 +10,7 @@
     colorBackground: '#ECEFF1',
     regionColor: 'rgba(255, 192, 192, 0.5)',
     infoColor: '#1B5E20',
-    infoFont: '10px Arial'
+    infoFont: '12px Arial'
   };
 
 
@@ -25,6 +25,10 @@
     this.canvas = document.getElementById(canvasId);
     this.ctx = this.canvas.getContext('2d');
 
+    // Offscreen canvas and context.
+    this.canvasOS = document.createElement('canvas');
+    this.ctxOS = this.canvasOS.getContext('2d');
+
     // Global style.
     this.ctx.font = STYLE.infoFont;
 
@@ -32,6 +36,7 @@
     this.controller = controller;
 
     this.renderedBuffer = null;
+    this.isBufferDirty = false;
     this.needsRedraw = false;
 
     // Start/end points on audio sample.
@@ -50,59 +55,74 @@
   MiniMap.prototype.clearCanvas = function () {
     this.ctx.fillStyle = STYLE.colorBackground;
     this.ctx.fillRect(0, 0, this.width, STYLE.height);
+
+    if (this.isBufferDirty) {
+      this.ctxOS.fillStyle = STYLE.colorBackground;
+      this.ctxOS.fillRect(0, 0, this.width, STYLE.height);
+    }
   };
 
   MiniMap.prototype.drawMiniMap = function () {
     if (!this.renderedBuffer)
       return;
 
-    // var pixelPerSample = this.width / this.renderedBuffer.length;
-    var y_origin = STYLE.miniMapHeight * 0.5;
-    var y_length;
-    var x = 0, px = 0;
+    // Draw mini map to offscreen when it's dirty.
+    if (this.isBufferDirty) {
+      var y_origin = STYLE.miniMapHeight * 0.5;
+      var y_length;
+      var x = 0, px = 0;
 
-    // Styles.
-    this.ctx.strokeStyle = STYLE.color;
+      // Styles.
+      this.ctxOS.strokeStyle = STYLE.color;
 
-    // Draw center line.
-    this.ctx.beginPath();
-    this.ctx.lineWidth = 0.5;
-    this.ctx.moveTo(0, y_origin);
-    this.ctx.lineTo(this.width, y_origin);
-    this.ctx.stroke();
+      // Draw center line.
+      this.ctxOS.beginPath();
+      this.ctxOS.lineWidth = 0.5;
+      this.ctxOS.moveTo(0, y_origin);
+      this.ctxOS.lineTo(this.width, y_origin);
+      this.ctxOS.stroke();
 
-    // Draw waveform.
-    this.ctx.beginPath();
-    this.ctx.lineWidth = 1.0;
-    
-    // TO FIX: use both channel.
-    var data = this.renderedBuffer.getChannelData(0);
-    var posSample = 0.0, negSample = 0.0, sample;
-    var maxSampleIndex;
-    var y_posOffset, y_negOffset;
+      // Draw waveform.
+      this.ctxOS.beginPath();
+      this.ctxOS.lineWidth = 1.0;
 
-    for (var i = 0; i < data.length; i++) {
-      // Pick each sample from positive and negative values.
-      sample = data[i];
-      if (sample > posSample)
-        posSample = sample;
-      else if (sample < negSample)
-        negSample = sample;
-      // Draw a line when it passes one pixel boundary.
-      if (x - px >= 1) {
-        y_posOffset = (1 - posSample) * y_origin;
-        y_negOffset = (1 - negSample) * y_origin;
-        
-        this.ctx.moveTo(x, y_posOffset);
-        this.ctx.lineTo(x, y_origin);
-        this.ctx.lineTo(x, y_negOffset);
-        
-        posSample = negSample = 0.0;
-        px = x;
+      // TO FIX: use both channel.
+      var data = this.renderedBuffer.getChannelData(0);
+      var posSample = 0.0, negSample = 0.0, sample;
+      var maxSampleIndex;
+      var y_posOffset, y_negOffset;
+
+      for (var i = 0; i < data.length; i++) {
+        // Pick each sample from positive and negative values.
+        sample = data[i];
+        if (sample > posSample)
+          posSample = sample;
+        else if (sample < negSample)
+          negSample = sample;
+        // Draw a line when it passes one pixel boundary.
+        if (x - px >= 1) {
+          posSample = Math.min(1.0, posSample);
+          negSample = Math.max(-1.0, negSample);
+
+          y_posOffset = (1 - posSample) * y_origin;
+          y_negOffset = (1 - negSample) * y_origin;
+
+          this.ctxOS.moveTo(x, y_posOffset);
+          this.ctxOS.lineTo(x, y_origin);
+          this.ctxOS.lineTo(x, y_negOffset);
+
+          posSample = negSample = 0.0;
+          px = x;
+        }
+        x += this.pixelPerSample;
       }
-      x += this.pixelPerSample;
+      this.ctxOS.stroke();
+
+      this.isBufferDirty = false;
     }
-    this.ctx.stroke();
+
+    // Copy from OS.
+    this.ctx.drawImage(this.ctxOS.canvas, 0, 0, this.width, STYLE.height);
   };
 
   MiniMap.prototype.drawOverlay = function () {
@@ -120,7 +140,7 @@
       this.isRegionValid = false;
       return;
     }
-    
+
     this.isRegionValid = true;
 
     this.ctx.fillStyle = STYLE.regionColor;
@@ -129,12 +149,19 @@
     this.ctx.fillStyle = STYLE.infoColor;
     this.ctx.fillRect(start, 0, 1, STYLE.miniMapHeight);
     this.ctx.fillRect(end - 1, 0, 1, STYLE.miniMapHeight);
-    this.ctx.fillText(sampleLength, start + (end - start) * 0.5, STYLE.miniMapHeight * 0.8);
+
 
     // Draw additional info above 150 pixel-width.
     if (end - start > 50) {
-      this.ctx.fillText(this.sampleStart, start, STYLE.miniMapHeight * 1.2);
-      this.ctx.fillText(this.sampleEnd, end, STYLE.miniMapHeight * 1.2);
+      this.ctx.fillText(this.getTimeFromSample(sampleLength),
+        start + (end - start) * 0.5, STYLE.miniMapHeight * 0.8);
+      this.ctx.fillText(this.getTimeFromSample(this.sampleStart),
+        start, STYLE.miniMapHeight * 1.2);
+      this.ctx.fillText(this.getTimeFromSample(this.sampleEnd),
+        end, STYLE.miniMapHeight * 1.2);
+    } else {
+      this.ctx.fillText(this.getTimeFromSample(sampleLength),
+        start + (end - start) * 0.5, STYLE.miniMapHeight * 1.2);
     }
 
     this.ctx.beginPath();
@@ -158,7 +185,7 @@
   MiniMap.prototype.updateViewPort = function () {
     if (!this.renderedBuffer)
       return;
-    
+
     // Change the PPS factor on resize, or buffer change.
     this.pixelPerSample = this.width / this.renderedBuffer.length;
     this.needsRedraw = true;
@@ -169,16 +196,19 @@
     this.sampleStart = 0;
     this.sampleEnd = this.renderedBuffer.length;
 
+    // Buffer just newly assigned. Mark it dirty.
+    this.isBufferDirty = true;
+
     // By default, the region is invalid. (start === end)
     this.isRegionValid = false;
-    
+
     this.updateViewPort();
   };
 
 
   // minimap -> controller
   MiniMap.prototype.setRegionRange = function (regionStart, regionEnd) {
-    
+
     // New start/end positions in samples.
     var start = Math.round(regionStart / this.pixelPerSample);
     var end = Math.round(regionEnd / this.pixelPerSample);
@@ -235,6 +265,8 @@
   MiniMap.prototype.onResize = function () {
     this.canvas.width = this.width = window.innerWidth - 520;
     this.canvas.height = STYLE.height;
+    this.canvasOS.width = this.width;
+    this.canvasOS.height = STYLE.height;
     this.updateViewPort();
   };
 
@@ -255,10 +287,10 @@
         switch (action) {
 
           case 'clicked':
-            
+
             px = data.x;
             dx = 0;
-            
+
             // Define behavior. Move only when the region is valid.
             if (start <= data.x && data.x <= end && this.isRegionValid) {
               mode = 'MOVE';
@@ -287,6 +319,15 @@
 
   };
 
+  MiniMap.prototype.getTimeFromSample = function (samples) {
+    if (!this.renderedBuffer)
+      return null;
+
+    var seconds = samples / this.renderedBuffer.sampleRate;
+    var sec = Math.floor(seconds);
+    var msec = Math.floor((seconds - sec) * 10000);
+    return sec + ':' + msec;
+  };
 
   // MiniMap factory.
   Canopy.createMiniMap = function (canvasId) {
